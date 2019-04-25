67812a4414048bf945a827c867c84d0aa3b51320
Greg Murashige <gregmurashige@gmail.com>
2019-04-25 13:25:48 -0700

    Tic-Tac-Toe Added most game logic.  Just have to finish looking for bugs.

--- practice/HelloWorld/src/TicTacToe/Board.kt
+++ practice/HelloWorld/src/TicTacToe/Board.kt
@@ -1,6 +1,59 @@
 package TicTacToe
 
-data class Board(val board: List<MutableList<Int>> = listOf(mutableListOf(2,2,2), mutableListOf(2,2,2),
-    mutableListOf(2,2,2))){
+data class Board(val board: List<MutableList<Int>> = listOf(mutableListOf(4,4,4), mutableListOf(4,4,4),
+    mutableListOf(4,4,4))){
+    private val sumL = listOf(
+        {board[0].sum()},
+        {board[1].sum()},
+        {board[2].sum()},
+        {board[0][0]+board[1][0]+board[2][0]},
+        {board[0][1]+board[1][1]+board[2][1]},
+        {board[0][2]+board[1][2]+board[2][2]},
+        {board[0][0]+board[1][1]+board[2][2]},
+        {board[0][2]+board[1][1]+board[2][0]})
 
+    fun gameOver(): Boolean{
+        return board.none { it.contains(4) } || sumL.any{it() == 0 || it() == 3}
+    }
+
+    fun getWinner(): String{
+        val oWinner = sumL.any{it() == 0}
+        val xWinner = sumL.any{it() == 3}
+        return when{
+            oWinner -> "O"
+            xWinner -> "X"
+            else -> "T"
+        }
+    }
+
+    fun display(){
+        println("+ - + - + - +\n" +
+                "| ${pixel(board[0][0])} | ${pixel(board[0][1])} | ${pixel(board[0][2])} |\n" +
+                "+ - + - + - +\n" +
+                "| ${pixel(board[1][0])} | ${pixel(board[1][1])} | ${pixel(board[1][2])} |\n" +
+                "+ - + - + - +\n" +
+                "| ${pixel(board[2][0])} | ${pixel(board[2][1])} | ${pixel(board[2][2])} |\n" +
+                "+ - + - + - +")
+    }
+    private inline fun pixel(p: Int): String{
+        return when(p){
+            0 -> "O"
+            1 -> "X"
+            else -> " "
+        }
+    }
+    private inline fun assign(player: Boolean) = if (player) 0 else 1
+    fun makeMove(player: Boolean){
+        println("Player ${if(player) "O" else "X"}'s turn, please make a move: ")
+        val move = readLine()?.map{it.toString().toInt()}?.take(2)
+        if (isValidMove(move)) {
+            board[move!![1]][move[0]] = assign(player)
+        }else{
+            println("Invalid Input, please try again.")
+            makeMove(player)
+        }
+    }
+    private inline fun isValidMove(move: List<Int>?): Boolean{
+        return move?.run{ move[0] in 0..2 && move[1] in 0..2 && board[move[1]][move[0]] == 4} ?: false
+    }
 }
--- practice/HelloWorld/src/TicTacToe/Main.kt
+++ practice/HelloWorld/src/TicTacToe/Main.kt
@@ -1,5 +1,7 @@
 package TicTacToe
 
+import java.util.*
+
 fun main(args: Array<String>){
     if (args.isEmpty()) {
         var runGame = true
@@ -21,14 +23,19 @@ fun main(args: Array<String>){
 
             winner = startGame(initializeBoard())
             when (winner){
-                "O" -> println("Player O has won the game!\n")
-                "X" -> println("Player X haas won the game!\n")
+                "X" -> {
+                    println("Player X has won the game!\n")
+                    history[0]++
+                }
+                "O" -> {
+                    println("Player O has won the game!\n")
+                    history[1]++
+                }
                 else -> println("Game is a Tie!\n")
             }
         }
         println("X player won ${history[0]} games\n" +
                 "O player won ${history[1]} games\n")
-
     }else{
 
     }
@@ -37,13 +44,13 @@ fun main(args: Array<String>){
 fun initializeRow(): MutableList<Int>{
      return readLine()
         ?.map{if (it == '0' || it == '1') it.toString().toInt() else 2}
-        ?.plus(listOf(2,2,2))!!
+        ?.plus(listOf(4,4,4))!!
         .take(3)
         .toMutableList()
 }
 
 fun initializeBoard(): Board{
-    print("***Starting game***\n\n - Would you like to add your own board-state? (Y/N): ")
+    print("***Starting game***\nWould you like to input your own board-state? (Y/N): ")
     return when(readLine()?.toLowerCase()){
         "y" -> {
             println("Enter the board state: format is 3 ints per row.  0's represent X's and 1's represent 0's, anything else will be treated as blank.\nEntering nothing in row will default row to blank and only the first 3 characters will be counted towards row")
@@ -55,13 +62,29 @@ fun initializeBoard(): Board{
             val row3 = initializeRow()
             Board(listOf(row1,row2,row3))
         }
+        "n" -> Board()
         else -> {
+            println("Invalid Input, preparing default board")
             Board()
         }
     }
 }
 
 fun startGame(board: Board): String{
-    println(board)
-    return "O"
+    print("Who will go first? (O/X/R): ")
+    var player = when(readLine()?.toLowerCase()){
+        "o" -> true
+        "x" -> false
+        "r" -> Random().nextBoolean()
+        else -> {
+            println("Invalid Input a Random Player will be chosen.")
+            Random().nextBoolean()
+        }
+    }
+    while (!board.gameOver()){
+        board.display()
+        board.makeMove(player)
+        player = !player
+    }
+    return board.getWinner()
 }
