adbf0e7090227977dcc39711c93702b7b75377c8
Greg Murashige <gregmurashige@gmail.com>
2019-04-25 09:32:00 -0700

    final build of graphics

--- practice/HelloWorld/src/GraphicalComparison.kt
+++ practice/HelloWorld/src/GraphicalComparison.kt
@@ -6,11 +6,11 @@ import java.io.File
     This is comprehensive research experiment to explore the graphical comparisons of different common resolutions.
  */
 
-fun test720p(out: BufferedWriter): () -> Unit = { for (i in 1..720){out.write("p"); if (i % 100 == 0) out.write("\n")}}
+fun test720p(out: BufferedWriter){ for (i in 1..720){out.write("p"); if (i % 90 == 0) out.write("\n")}}
 
-fun test1080p(out: BufferedWriter): () -> Unit = { for (i in 1..1080){out.write("p"); if (i % 100 == 0) out.write("\n")}}
+fun test1080p(out: BufferedWriter){ for (i in 1..1080){out.write("p"); if (i % 120 == 0) out.write("\n")}}
 
-fun test1080i(out: BufferedWriter): () -> Unit = { for (i in 1..1080){out.write("i"); if (i % 100 == 0) out.write("\n")}}
+fun test1080i(out: BufferedWriter){ for (i in 1..1080){out.write("i"); if (i % 120 == 0) out.write("\n")}}
 
 fun test4k(out: BufferedWriter) = repeat(4){out.write("K")}
 fun main(){
@@ -19,13 +19,13 @@ fun main(){
 
     myfile.bufferedWriter().use { out ->
 
-        out.write("720p - ")
+        out.write("720p\n")
+        test720p(out)
+        out.write("\n\n1080p\n")
         test1080p(out)
-        out.write("\n\n1080p - ")
-        test1080p(out)
-        out.write("\n\n1080i - ")
+        out.write("\n\n1080i\n")
         test1080i(out)
-        out.write("\n\n4K - ")
+        out.write("\n\n4K\n")
         test4k(out)
     }
 }
