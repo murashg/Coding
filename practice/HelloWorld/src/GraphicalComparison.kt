0d5c279044a2de15fd4aed86bc2f228cd8954702
Greg Murashige <gregmurashige@gmail.com>
2019-04-25 09:26:27 -0700

    format comparison

--- practice/HelloWorld/src/GraphicalComparison.kt
+++ practice/HelloWorld/src/GraphicalComparison.kt
@@ -6,15 +6,15 @@ import java.io.File
     This is comprehensive research experiment to explore the graphical comparisons of different common resolutions.
  */
 
-fun test720p(out: BufferedWriter) = repeat(720){out.write("p")}
+fun test720p(out: BufferedWriter): () -> Unit = { for (i in 1..720){out.write("p"); if (i % 100 == 0) out.write("\n")}}
 
-fun test1080p(out: BufferedWriter) = repeat(1080){out.write("p")}
+fun test1080p(out: BufferedWriter): () -> Unit = { for (i in 1..1080){out.write("p"); if (i % 100 == 0) out.write("\n")}}
 
-fun test1080i(out: BufferedWriter) = repeat(1080){out.write("i")}
+fun test1080i(out: BufferedWriter): () -> Unit = { for (i in 1..1080){out.write("i"); if (i % 100 == 0) out.write("\n")}}
 
 fun test4k(out: BufferedWriter) = repeat(4){out.write("K")}
 fun main(){
-    val fileName = "src/resources/myfile.txt"
+    val fileName = "src/resources/graphicalComparison.txt"
     val myfile = File(fileName)
 
     myfile.bufferedWriter().use { out ->
