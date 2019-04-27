e19143c84f1f2548617089817e4d8535be968b74
Greg Murashige <gregmurashige@gmail.com>
2019-04-27 08:17:26 -0700

    Down to 4 lines

--- practice/HelloWorld/src/uniqueChar.kt
+++ practice/HelloWorld/src/uniqueChar.kt
@@ -9,7 +9,8 @@ fun main() {
 
 private fun findFirstUniqueChar(input: String): String{
     if (input.isEmpty()) return "No Unique Chars"
-    val map = mutableMapOf<Char, Int>() //linkedHashMap maintains insertion order
-    for (c in input) map[c] = map[c]?.plus(1) ?: 1
-    return map.entries.find { (_, v) -> v < 2 }?.key?.toString() ?: "No Unique Chars"
+    val map = mutableMapOf<Char, Int>() //linkedHashMap maintains insertion order (character,count)
+    for (c in input) map[c] = map[c]?.plus(1) ?: 1 //add one to map entry or create entry with val=1
+    return map.entries.find { (_, v) -> v < 2 }?.key?.toString()
+        ?: "No Unique Chars" //get first character where count < 2, if none exist return default
 }
