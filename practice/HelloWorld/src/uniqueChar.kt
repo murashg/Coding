89ef1aa4c30e7b8ab8412e820fdbe12fd20a8e5e
Greg Murashige <gregmurashige@gmail.com>
2019-04-27 08:15:31 -0700

    Down to 4 lines

--- practice/HelloWorld/src/uniqueChar.kt
+++ practice/HelloWorld/src/uniqueChar.kt
@@ -9,16 +9,7 @@ fun main() {
 
 private fun findFirstUniqueChar(input: String): String{
     if (input.isEmpty()) return "No Unique Chars"
-    val uniqueChars = mutableSetOf<Char>() //linkedHashSet maintains insertion order
-    val duplicateChars = hashSetOf<Char>()
-    for (c in input){
-        if (!duplicateChars.contains(c)){
-            uniqueChars += c // "+=" == .plusAssign() == .add()
-            duplicateChars += c
-        }else{
-            uniqueChars -= c
-        }
-    }
-    //return minimum index entry key or if map is empty "No Unique Chars"
-    return if (uniqueChars.isNotEmpty()) uniqueChars.first().toString() else "No Unique Chars"
+    val map = mutableMapOf<Char, Int>() //linkedHashMap maintains insertion order
+    for (c in input) map[c] = map[c]?.plus(1) ?: 1
+    return map.entries.find { (_, v) -> v < 2 }?.key?.toString() ?: "No Unique Chars"
 }
